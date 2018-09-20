18b5c6bc644072f66b9bb01d59552e76da3c2fc6
Unknown <antonio.daniswara@gmail.com>
2018-09-21 08:55:26 +1000

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import torch
+
+
+# reading file
+words_file = open("data/dev.txt","r")
+print(words_file.read())
+
+# with open("data/dev.txt","r") as word_file:
+#     for line in word_file:
+#         print( line)
